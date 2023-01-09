a786e69e368b1dd36d2b259c020f889c4d3f6707
Rafael13m <rafael13m@hotmail.com>
2023-01-09 16:21:16 -0300

    commit incial

---
+++ src/main/java/com/attornatuspessoas/AttornatusPessoasApplication.java
@@ -0,0 +1,13 @@
+package com.attornatuspessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttornatusPessoasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AttornatusPessoasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/attornatuspessoas/AttornatusPessoasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attornatuspessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttornatusPessoasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
